064fbc1f5e76f116fb95041890e71c7518bb4a78
iceinmyeye <iceinmyeye@163.com>
2018-02-06 14:29:35 +0800

    新增部分练习

---
+++ Algorithms/src/com/adt/Practice.java
@@ -0,0 +1,14 @@
+package com.adt;
+
+public class Practice {
+	
+	public static void main(String[] args) {
+		// 1.2.4
+		String string1 = "hello";
+		String string2 = string1;
+		string1 = "world";
+		System.out.println(string1);
+		System.out.println(string2);
+	}
+
+}
